a82fe2b1b1479e00bec15b068f0117f8934a60ba
yacj <1356888297@qq.com>
2020-04-28 09:06:46 +0800

    mall

---
+++ src/views/order/OrderDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="OrderDetail">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OrderDetail",
+        data() {
+            return {}
+        },
+        created() {
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
